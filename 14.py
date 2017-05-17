053bd71a5b87f150dd90e489389a8acc9c732372
bestatom <15135099395@163.com>
2017-05-17 22:41:46 +0800

    Update 14.py

--- 14.py
+++ 14.py
@@ -8,9 +8,8 @@ with open('D://pics//student.txt','r') as f:
     workbook = xlwt.Workbook()
     """如果对一个单元格重复操作，会引发returns error:
     Exception: Attempt to overwrite cell:
-    sheetname=u'sheet 1' rowx=0 colx=0"""
-
-所以在打开时加cell_overwrite_ok=True解决
+    sheetname=u'sheet 1' rowx=0 colx=0
+    所以在打开时加cell_overwrite_ok=True解决"""
     sheet1 = workbook.add_sheet('student', cell_overwrite_ok=True)
     for index, (key, values) in enumerate(data.items()):
         sheet1.write(index, 0, key)
