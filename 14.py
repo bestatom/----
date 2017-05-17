e0858bfc8caf021fdccac300f1387e3496caddef
bestatom <15135099395@163.com>
2017-05-17 22:41:16 +0800

    Update 14.py

--- 14.py
+++ 14.py
@@ -6,6 +6,11 @@ import xlwt,json
 with open('D://pics//student.txt','r') as f:
     data = json.load(f, object_pairs_hook=OrderedDict)
     workbook = xlwt.Workbook()
+    """如果对一个单元格重复操作，会引发returns error:
+    Exception: Attempt to overwrite cell:
+    sheetname=u'sheet 1' rowx=0 colx=0"""
+
+所以在打开时加cell_overwrite_ok=True解决
     sheet1 = workbook.add_sheet('student', cell_overwrite_ok=True)
     for index, (key, values) in enumerate(data.items()):
         sheet1.write(index, 0, key)
